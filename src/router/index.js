75f9b0c3ba84a48a8e8d3220069ef5fee6454515
ko566274 <583011996@qq.com>
2018-10-16 14:32:51 +0800

    v1.1

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import home from '../components/home.vue'
+import find from '../components/find.vue'
+import shopping from '../components/shopping.vue'
+import store from '../components/store.vue'
+import login from '../components/login.vue'
 Vue.use(Router)
 
 export default new Router({
