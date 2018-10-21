1a1b0825e0c68d126c87260907d5b3aa659689bb
“zha <2746895955@qq.com>
2018-10-22 00:08:39 +0800

    ？？？

--- src/router/index.js
+++ src/router/index.js
@@ -15,6 +15,10 @@ import store from '../components/store.vue'
 import login from '../components/login.vue'
 import cinemadetail from '../components/cinemadetail.vue'
 
+import news from '../components/news.vue'
+import dvance from '../components/dvance.vue'
+import rank from '../components/rank.vue'
+import search from '../components/search.vue'
 Vue.use(Router)
 
 export default new Router({
@@ -36,10 +40,43 @@ export default new Router({
                 }
             ]
         },
-        {
-            path: '/find',
-            component: find
-        },
+      {
+        path: '/find',
+        name: "find",
+        component: find,}
+      ,
+      {
+        path: '/find',
+        name: "find",
+        component: find,
+        children:[
+          {
+            path:"news",
+            name: "news",
+            component:news
+          },
+          {
+            path:"dvance",
+            name: "dvance",
+            component:dvance
+          },
+          {
+            path:"rank",
+            name: "rank",
+            component:rank
+          },
+          {
+            path:"search",
+            name: "search",
+            component:search
+          },
+          {
+            path:"/",
+            redirect:"/find/news"
+          }
+        ]
+      },
+
         {
             path: '/shopping',
             name: "shopping",
@@ -47,7 +84,8 @@ export default new Router({
         },
         {
             path: '/shopping',
-            component: shopping
+            component: shopping,
+
         },
         {
             path: '/store',
